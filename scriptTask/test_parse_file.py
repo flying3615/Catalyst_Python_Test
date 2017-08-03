94c4230d21e99b7931dae2de790a8744a6108333
liuyufei <gabriel.liu3615@gmail.com>
2017-08-04 00:04:22 +1200

    unit test, file path relative

--- scriptTask/test_parse_file.py
+++ scriptTask/test_parse_file.py
@@ -4,6 +4,6 @@ from user_upload import parse_file
 class TestParse_file(TestCase):
 
     def test_parse_file(self):
-        header, values = parse_file("/Users/liuyufei/Documents/Learn/Catalyst_Python_Test/users.csv")
+        header, values = parse_file("./users.csv")
         self.assertEqual(header,['name','surname','email\t'])
         self.assertIsNotNone(values)
