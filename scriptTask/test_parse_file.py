273f50140e3638f02be3c2ff6e0045740b2582bd
liuyufei <gabriel.liu3615@gmail.com>
2017-08-05 00:10:05 +1200

    add test expectation for insert statements

--- scriptTask/test_parse_file.py
+++ scriptTask/test_parse_file.py
@@ -11,4 +11,16 @@ class UT_file(TestCase):
     def test_construct_statements(self):
         header, values = parse_file("./users.csv")
         statements = construct_statements(header, values)
-        self.assertIsNotNone(statements)
+        expected_statement = [
+            'INSERT INTO users (name,surname,email) VALUES ("John","Smith","jsmith@gmail.com")',
+            'INSERT INTO users (name,surname,email) VALUES ("Hamish","Jones","ham@seek.com")',
+            'INSERT INTO users (name,surname,email) VALUES ("Phil","Carry","phil@open.edu.au")',
+            'INSERT INTO users (name,surname,email) VALUES ("Johnny","O\'Hare","john@yahoo.com.au")',
+            'INSERT INTO users (name,surname,email) VALUES ("Mike","O\'Connor","mo\'connor@cat.net.nz")',
+            'INSERT INTO users (name,surname,email) VALUES ("William","Smythe","happy@ent.com.au")',
+            'INSERT INTO users (name,surname,email) VALUES ("Hamish","Jones","ham@seek.com")',
+            'INSERT INTO users (name,surname,email) VALUES ("Sam!!","Walters","sam!@walters.org")',
+            'INSERT INTO users (name,surname,email) VALUES ("Daley","Thompson","daley@yahoo.co.nz")',
+            'INSERT INTO users (name,surname,email) VALUES ("Kevin","Ruley","kevin.ruley@gmail.com")',
+        ]
+        self.assertEqual(statements,expected_statement)
